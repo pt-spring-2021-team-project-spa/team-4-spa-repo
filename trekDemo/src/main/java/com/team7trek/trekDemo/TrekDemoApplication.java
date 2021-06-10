b1b77e96dab515dcd6e202f30c15a2ed910c9909
Jovahnn Tucker <Jovahnn@yahoo.com>
2021-06-10 16:25:07 -0400

    New Dependencies

---
+++ trekDemo/src/main/java/com/team7trek/trekDemo/TrekDemoApplication.java
@@ -0,0 +1,13 @@
+package com.team7trek.trekDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrekDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrekDemoApplication.class, args);
+	}
+
+}
---
+++ trekDemo/src/test/java/com/team7trek/trekDemo/TrekDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team7trek.trekDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrekDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
