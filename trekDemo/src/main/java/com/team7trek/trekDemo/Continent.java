551efa605ae786bfcab281700c6f1fdfbce2d221
Jovahnn Tucker <Jovahnn@yahoo.com>
2021-06-12 12:04:37 -0400

    Work on classes

--- trekDemo/src/main/java/com/team7trek/trekDemo/Continent.java
+++ trekDemo/src/main/java/com/team7trek/trekDemo/Continent.java
@@ -1,6 +1,15 @@
 package com.team7trek.trekDemo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Collection;
+
+@Entity
 public class Continent {
+
+    @Id
+    @GeneratedValue
 
 
     public Continent(String test_location) {
