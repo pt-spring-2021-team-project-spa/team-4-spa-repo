2320189a98a5239d2b21ad601a6527ebd9f9a51d
Jovahnn Tucker <Jovahnn@yahoo.com>
2021-06-18 14:30:35 -0400

    code fix

--- trekDemo/src/test/java/com/team7trek/trekDemo/JPAWiringTest.java
+++ trekDemo/src/test/java/com/team7trek/trekDemo/JPAWiringTest.java
@@ -10,8 +10,9 @@ public class JPAWiringTest  {
 
     @Autowired
     private TestEntityManager entityManager;
+
     @Autowired
-    private ContinentRepository continentRepository;
+    private ContinentRepository continentRepo;
     @Autowired
     private RegionRepository regionRepo;
     @Autowired
@@ -23,7 +24,10 @@ public class JPAWiringTest  {
         Region testRegion1 = new Region("Test title","Test image","Test climate");
         Trek testTrek = new Trek("Test title","Test difficulty","Test description","Test review","Test image", testContinent,testRegion1);
 
+        continentRepo.save(testContinent);
         regionRepo.save(testRegion1);
+        trekRepo.save(testTrek);
+
 
     }
 }
