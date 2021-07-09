3bc2f76be369005e38af4f53b6045ce187ac4a55
alan <alan@wecancodeit.org>
2021-07-09 11:06:12 -0400

    initial project setup

---
+++ src/main/java/org/wecancodeit/mysteryeducator/MysteryEducatorApplication.java
@@ -0,0 +1,15 @@
+package org.wecancodeit.mysteryeducator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysteryEducatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysteryEducatorApplication.class, args);
+
+		System.out.println("Server Side is wired up!");
+	}
+
+}
---
+++ src/test/java/org/wecancodeit/mysteryeducator/MysteryEducatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wecancodeit.mysteryeducator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysteryEducatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
