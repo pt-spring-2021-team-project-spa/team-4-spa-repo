d8fa4d735ec9fab7fe1b3e8457ed8ccd6d9f603b
Jovahnn Tucker <Jovahnn@yahoo.com>
2021-07-11 16:42:30 -0400

    History and Planets

--- src/main/java/models/History.java
+++ src/main/java/models/History.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import java.util.Objects;
 
 @Entity
 public class History {
@@ -28,9 +29,26 @@ public class History {
         return synapse;
     }
 
+    public History() {
+
+    }
+
     public History(Long id, String bio, String synapse) {
         this.id = id;
         this.bio = bio;
         this.synapse = synapse;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        History history = (History) o;
+        return Objects.equals(id, history.id) && Objects.equals(bio, history.bio) && Objects.equals(synapse, history.synapse);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, bio, synapse);
     }
 }
