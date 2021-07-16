0b0ec1b54454ab48149cb4acb460a0cff2c44de1
vascoe1 <ugafilms@gmail.com>
2021-07-15 21:18:42 -0400

    This works

---
+++ Planets.js
@@ -0,0 +1,15 @@
+export default function Planets() {
+    return ' <
+        ul class = 'nav__list' >
+        <
+        li class = 'nav__list_Jupiter' > Jupiter < /li> <
+        li class = 'nav__list_Saturn' > Saturn < /li> <
+        li class = 'nav__list_Mars' > Mars < /li> <
+        li class = 'nav__list_Venus' > Venus < /li> <
+        li class = 'nav__list_Earth' > Earth < /li> <
+        li class = 'nav__list_Uranus' > Uranus < /li> <
+        li class = 'nav__list_Neptune' > Neputune < /li> <
+        li class = 'nav__list_Mercury' > Mercury < /li> <
+        /ul>
+    '; ....
+}
