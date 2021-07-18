66aff1ccd5d6abe244a03fd4720885e954f984fc
zacharytabler <zachary.tabler@gmail.com>
2021-07-18 15:31:43 -0400

    Updated commit

--- client-side/js/components/Planets.js
+++ client-side/js/components/Planets.js
@@ -1,6 +1,12 @@
 export default function Planets(planets) {
   return `
-  <h1>Planets</h1>
-  
-  `
-}
+    <h1>Planets</h1>
+    <ul class='planets-list'>
+    ${planets.map(planet => {
+        return `
+        <li class='planets-list__name'>${planet.name}</li>
+        `
+    }).join('')}
+    </ul>
+    `
+  }
