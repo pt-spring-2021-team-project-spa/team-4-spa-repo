8e4a09f4877cbf359ac27c3e90a326334c57f99e
zacharytabler <zachary.tabler@gmail.com>
2021-07-18 16:12:32 -0400

    Worked on bringing over the Home and About Us pages

--- client-side/js/components/About.js
+++ client-side/js/components/About.js
@@ -1,6 +1,80 @@
 import earthImg from "../../images/earth-photo.jpg";
 export default function About() {
   return `
-      <h1>About</h1>
+  <main>
+  <section class="title">
+         <h2>About Us</h2>
+         <p>"Space KIDet: Enjoy Space Like Never Before!"</p>
+  </section>
+  <section>
+    <div class="container">
+      <h2>Duwe Aynab</h2>
+      <h3>Command Module Pilot</h3>
+      <div class="imgs">
+        <img src="./images/" alt="Photo Of Duwe Aynab" />
+      </div>
+      <p>
+      Duwe Bio.
+      </p>
+      <a href="#" class="button">DuweAynab@SpaceKIDet.com</a>
+    </div>
+  </section>
+  <section>
+    <div class="container">
+      <h2>Miranda Perry</h2>
+      <h3>Lunar Module Pilot</h3>
+      <div class="imgs">
+        <img src="./images/" alt="Photo Of Miranda Perry" />
+      </div>
+      <p>
+        Miranda Bio.
+      </p>
+      <a href="#" class="button">MirandaPerry@SpaceKIDet.com</a>
+    </div>
+  </section>
+  <section>
+  <div class="container">
+      <h2>Alicia Sanders</h2>
+      <h3>Mission Specialist</h3>
+      <div class="imgs">
+        <img src="./images/" alt="Photo Of Alicia Sanders" />
+      </div>
+      <p>
+        Alicia Bio.
+      </p>
+      <a href="#" class="button">AliciaSanders@SpaceKIDet.com</a>
+    </
+    <div class="container">
+      <h2>Zachary Tabler</h2>
+      <h3>Flight Engineer</h3>
+      <div class="imgs">
+        <img
+          src="./images/"
+          alt="Photo Of Zachary Tabler"
+        />
+      </div>
+      <p>
+        Zach Bio.
+      </p>
+      <a href="#" class="button">ZacharyTabler@SpaceKIDet.com</a>
+    </div>
+  </section>
+  <section>
+    div>
+  </section>
+  <section>
+    <div class="container">
+      <h2>Jovahnn Tucker</h2>
+      <h3>Commander</h3>
+      <div class="imgs">
+        <img src="images/space-kids.jpg" alt="Photo Of Jovahnn Tucker" />
+      </div>
+      <p>
+        Jovahnn Bio. 
+      </p>
+      <a href="#" class="button">JovahnnTucker@SpaceKIDet.com</a>
+    </div>
+  </section>
+</main>
       `;
 }
