63f488a10e4f52769a1f8b0bcf6a607ef2dd0f5b
MirandaPerry1982 <mirandaperry1982@yahoo.com>
2021-07-19 19:20:43 -0400

    About us page styling

--- client-side/js/components/About.js
+++ client-side/js/components/About.js
@@ -6,73 +6,83 @@ export default function About() {
          <h2>About Us</h2>
          <p>"Space KIDet: Enjoy Space Like Never Before!"</p>
   </section>
+
   <section>
     <div class="container">
       <h2>Duwe Aynab</h2>
       <h3>Command Module Pilot</h3>
       <div class="imgs">
-        <img src="./images/" alt="Photo Of Duwe Aynab" />
+        <img src="./images/DuweAynab.png" alt="Photo Of Duwe Aynab">
       </div>
       <p>
-      Duwe Bio.
+      Duwe Aynab brings creative solutions to the team with his full stack web development skills. 
+      Commited to clean code and agile practices, he consistently delivers efficient and optimal solutions.
       </p>
-      <a href="#" class="button">DuweAynab@SpaceKIDet.com</a>
+      <a href="https://www.linkedin.com/in/duwe-aynab-b1a764212/" class="button">Linkedin Profile</a>
     </div>
   </section>
+
   <section>
     <div class="container">
       <h2>Miranda Perry</h2>
       <h3>Lunar Module Pilot</h3>
       <div class="imgs">
-        <img src="./images/" alt="Photo Of Miranda Perry" />
+        <img src="./images/Miranda.jpg" alt="Photo Of Miranda Perry">
       </div>
       <p>
-        Miranda Bio.
+      Miranda Perry is a full stack web developer in Cleveland, Ohio.  
+      She bring creative solutions and time management skills to the table. She enjoys creating things 
+      from the ground up, working in teams and problem solving. With a passion for learning new technologies, 
+      she can't wait to see what the future will bring.
+
       </p>
-      <a href="#" class="button">MirandaPerry@SpaceKIDet.com</a>
+      <a href="https://www.linkedin.com/in/mirandalperry/" class="button">Linkedin profile</a>
     </div>
   </section>
+
   <section>
   <div class="container">
       <h2>Alicia Sanders</h2>
       <h3>Mission Specialist</h3>
       <div class="imgs">
-        <img src="./images/" alt="Photo Of Alicia Sanders" />
+        <img src="./images/" alt="Photo Of Alicia Sanders">
       </div>
       <p>
-        Alicia Bio.
+      Alicia Sanders is a software Developer with experience in full-stack web development.
+      She is excited to enter the Software Development field and apply her excellent problem solving skills.
+      Her attention to detail and determination make her an asset to any team. 
       </p>
-      <a href="#" class="button">AliciaSanders@SpaceKIDet.com</a>
+      <a href="https://www.linkedin.com/in/alicia-sanders7/" class="button">Linkedin Profile</a>
     </section>
+
+    <section>
     <div class="container">
       <h2>Zachary Tabler</h2>
       <h3>Flight Engineer</h3>
       <div class="imgs">
-        <img
-          src="./images/"
-          alt="Photo Of Zachary Tabler"
-        />
+        <img src="./images/"alt="Photo Of Zachary Tabler">
       </div>
       <p>
-        Zach Bio.
+      Innovative software developer with an affinity towards back-end software development and testing. 
+      Zach has over 15 years of experience exceeding clients’ needs, and achieving business results. 
+      Zach is seeking opportunities with a forward-thinking tech company that will allow him to help develop software that makes an impact.
       </p>
-      <a href="#" class="button">ZacharyTabler@SpaceKIDet.com</a>
-    </div>
+      <a href="https://www.linkedin.com/in/zacharytabler/" class="button">Linkedin Profile</a>
   </section>
+  
+    
   <section>
-    </div>
-  </section>
     <div class="container">
       <h2>Jovahnn Tucker</h2>
       <h3>Commander</h3>
       <div class="imgs">
-        <img src="images/space-kids.jpg" alt="Photo Of Jovahnn Tucker" />
+        <img src="./images/Jovahnn.jpg" alt="Photo Of Jovahnn Tucker">
       </div>
       <p>
-        Jovahnn Bio. 
+      Jovahnn Tucker is an experienced and knowledgeable information technology professional with 4+ years’ experience.
+      He seeks to contribute his training and acquired skills within a technical support, programming or engineering role. 
       </p>
-      <a href="#" class="button">JovahnnTucker@SpaceKIDet.com</a>
-    </div>
+      <a href="https://www.linkedin.com/in/jovahnn-tucker/" class="button">Linkedin Profile</a>
   </section>
 </main>
       `;
