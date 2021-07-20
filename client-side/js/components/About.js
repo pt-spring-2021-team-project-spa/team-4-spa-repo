5d39c973c1447feb926a4d59836c3396e322d1c3
Jovahnn Tucker <Jovahnn@yahoo.com>
2021-07-19 20:58:27 -0400

    Pics and links added to about us

--- client-side/js/components/About.js
+++ client-side/js/components/About.js
@@ -11,12 +11,12 @@ export default function About() {
       <h2>Duwe Aynab</h2>
       <h3>Command Module Pilot</h3>
       <div class="imgs">
-        <img src="./images/" alt="Photo Of Duwe Aynab" />
+        <img src="https://media-exp1.licdn.com/dms/image/C4E03AQF2y5Ui9CbWkw/profile-displayphoto-shrink_200_200/0/1625940298272?e=1632355200&v=beta&t=7331p2MmGlVAgKoHt26Mo09fo5ufd8Loml1oHB8tJkk" alt="Photo Of Duwe Aynab" />
       </div>
       <p>
       Duwe Bio.
       </p>
-      <a href="#" class="button">DuweAynab@SpaceKIDet.com</a>
+      <a href="https://www.linkedin.com/in/duwe-aynab-b1a764212/" class="button">DuweAynab@SpaceKIDet.com</a>
     </div>
   </section>
   <section>
@@ -24,12 +24,12 @@ export default function About() {
       <h2>Miranda Perry</h2>
       <h3>Lunar Module Pilot</h3>
       <div class="imgs">
-        <img src="./images/" alt="Photo Of Miranda Perry" />
+        <img src="https://media-exp1.licdn.com/dms/image/C4E03AQGSpzMrqxZ1bw/profile-displayphoto-shrink_200_200/0/1623202004703?e=1632355200&v=beta&t=4vfas84gGLSlhKN3UzCanE_BCDOH3rywRKnburt-pok" alt="Photo Of Miranda Perry" />
       </div>
       <p>
         Miranda Bio.
       </p>
-      <a href="#" class="button">MirandaPerry@SpaceKIDet.com</a>
+      <a href="https://www.linkedin.com/in/mirandalperry/" class="button">MirandaPerry@SpaceKIDet.com</a>
     </div>
   </section>
   <section>
@@ -37,26 +37,26 @@ export default function About() {
       <h2>Alicia Sanders</h2>
       <h3>Mission Specialist</h3>
       <div class="imgs">
-        <img src="./images/" alt="Photo Of Alicia Sanders" />
+        <img src="https://media-exp1.licdn.com/dms/image/C4E03AQF1SkRw_YMS-w/profile-displayphoto-shrink_200_200/0/1623215171599?e=1632355200&v=beta&t=winlTjh6UmxfxawfzrKpzNQzroQaH9jC9Ie3ycRjBT4" alt="Photo Of Alicia Sanders" />
       </div>
       <p>
         Alicia Bio.
       </p>
-      <a href="#" class="button">AliciaSanders@SpaceKIDet.com</a>
+      <a href="https://www.linkedin.com/in/alicia-sanders7/" class="button">AliciaSanders@SpaceKIDet.com</a>
     </section>
     <div class="container">
       <h2>Zachary Tabler</h2>
       <h3>Flight Engineer</h3>
       <div class="imgs">
         <img
-          src="./images/"
+          src="https://media-exp1.licdn.com/dms/image/C4E03AQF-tpGwaclGMA/profile-displayphoto-shrink_200_200/0/1624046970482?e=1632355200&v=beta&t=E0iyTPlJsVApp7MuAV13mFbhiBK1-LXiQrJF4wraTEY"
           alt="Photo Of Zachary Tabler"
         />
       </div>
       <p>
         Zach Bio.
       </p>
-      <a href="#" class="button">ZacharyTabler@SpaceKIDet.com</a>
+      <a href="https://www.linkedin.com/in/zacharytabler/" class="button">ZacharyTabler@SpaceKIDet.com</a>
     </div>
   </section>
   <section>
@@ -66,12 +66,13 @@ export default function About() {
       <h2>Jovahnn Tucker</h2>
       <h3>Commander</h3>
       <div class="imgs">
-        <img src="images/space-kids.jpg" alt="Photo Of Jovahnn Tucker" />
+        <img src="https://media-exp1.licdn.com/dms/image/C5603AQGzL9hv3U9hIA/profile-displayphoto-shrink_200_200/0/1624129010525?e=1632355200&v=beta&t=9PAf-Q-YZdt7BxwUCykFwjQqljDm5PKgrF8LvI6cQ1A" 
+        alt="Photo Of Jovahnn Tucker" />
       </div>
       <p>
         Jovahnn Bio. 
       </p>
-      <a href="#" class="button">JovahnnTucker@SpaceKIDet.com</a>
+      <a href="https://www.linkedin.com/in/jovahnn-tucker/" class="button">JovahnnTucker@SpaceKIDet.com</a>
     </div>
   </section>
 </main>
