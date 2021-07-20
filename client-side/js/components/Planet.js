68bc747993c2e8ef59971613ca670cbe5b3e4ff5
asanders7 <alicia.sanders88@hotmail.com>
2021-07-20 18:38:14 -0400

    finished planet.js and planets.js components and added functions to the index.js file

--- client-side/js/components/Planet.js
+++ client-side/js/components/Planet.js
@@ -1,8 +1,29 @@
-import Planets from '../components/Planets';
+// import Planets from '../components/Planets';
 
-export default function Planet(planets) {
+// export default function Planet(planets) {
+//     return `
+//     <h1>Planet</h1>
+//     ${Planets(planet)}
+//     `;
+// }
+
+export default function Planet(planet) {
     return `
-    <h1>Planet</h1>
-    ${Planets(planet)}
-    `;
+        <h1>${planet.name}</h1>
+        <h2>${planet.description}</h2>
+        <h2>${planet.distance}</h2>
+        <h2>${planet.size}</h2>
+        <div>
+        ${planet.history.map(history => {
+            return `
+                <h2>
+                    Bio: <span>${history.bio}</span>
+                </h2>
+                <h2>
+                    Synopsis: <span>${history.synopsis}</span>
+                </h2>
+                    `
+        }).join('')}
+        </div>
+    `
 }
