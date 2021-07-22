d13a2fbdcdfe1a68671ca3e09ea1e42c3b1449a7
zacharytabler <zachary.tabler@gmail.com>
2021-07-22 16:43:26 -0400

    disregard branch

--- client-side/js/components/Footer.js
+++ client-side/js/components/Footer.js
@@ -1,5 +1,8 @@
 export default function Footer() {
-    return `
+  return `
+                <section class="footer__section_copy">
+                    <h4 class="footer__section_text">Vincit Qui Se Vincit</h4>
         <small class="footer">&copy JacKr Enterprises 2021</small>
+        </section>
     `;
 }
--- client-side/js/index.js
+++ client-side/js/index.js
@@ -44,6 +44,7 @@ function HomePage() {
 function quizPage() {
   const quizElem = document.querySelector(".nav-list__quiz");
   quizElem.addEventListener("click", () => {
+    const app = document.querySelector("#app");
     app.innerHTML = Quiz();
   });
 }
@@ -106,3 +107,87 @@ function renderPlanetInfo() {
               }, (planet) => app.innerHTML = Planet(planet));
           }
       })
+
+      var numQues = 15;
+
+      var numChoi = 3;
+      var i = i;
+      var scorePer = scorePer;
+      getScore = false && getScore();
+      var answers = new Array(15);
+      
+      answers[0] = "doesn't like";
+      
+      answers[1] = "don't come";
+      
+      answers[2] = "come";
+      
+      answers[3] = "don't";
+      
+      answers[4] = "doesn't make";
+      
+      answers[5] = "seem";
+      
+      answers[6] = "don't have";
+      
+      answers[7] = "doesn't have";
+      
+      answers[8] = "takes";
+      
+      answers[9] = "want";
+      
+      answers[10] = "comes";
+      
+      answers[11] = "doesn't know";
+      
+      answers[12] = "tries";
+      
+      answers[13] = "play";
+      
+      answers[14] = "lasts";
+      
+      function getScore(form) {
+      
+      var score = 0;
+      
+      var currElt;
+      
+      var currSelection;
+      
+      for (i=0; i<numQues; i++) {
+      
+      currElt = i*numChoi;
+      
+      answered=false;
+      
+      for (j=0; j<numChoi; j++) {
+      
+      currSelection = form.elements[currElt + j];
+      
+      if (currSelection.checked) {
+      
+      answered=true;
+      
+      if (currSelection.value == answers[i]) {
+      
+      score++;
+      
+      break;
+      
+      }
+      
+      }
+      
+      }
+      
+      if (answered ===false){alert("Do answer all the questions, Please") ;return false;}
+      
+      }
+      
+      var scorePer = Math.round(score/numQues*100);
+      
+      form.percentage.value = scorePer + "%";
+      
+      form.mark.value=score;
+      
+      }
