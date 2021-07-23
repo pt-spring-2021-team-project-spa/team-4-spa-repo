ccd656dae9ff09be22f2cff90271b881a8528f27
asanders7 <alicia.sanders88@hotmail.com>
2021-07-23 18:13:19 -0400

    data added from smithsonian api

--- client-side/js/Pages/SmithsonianArt.js
+++ client-side/js/Pages/SmithsonianArt.js
@@ -1,7 +1,21 @@
+// import planetarySystemImg from '../../images/beautyofheavens.jpg';
+
 export default function SmithsonianPage(info) {
     console.log(info);
     return `
-      <h1>Planet Art From Smithsonian API</h1>
+      <h1>Available Book From Smithsonian Library:</h1>
+
+      <h2>Book Title</h2>
       ${info.response.title};
+
+      <h2>Author</h2>
+      ${info.response.content.indexedStructured.name};
+
+      <h2>Topic</h2>
+      ${info.response.content.indexedStructured.topic};
+     
+     <h2>Language</h2>
+      ${info.response.content.indexedStructured.language};
+
       `;
   }
